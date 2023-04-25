d558ca0475788fbde272a2d1469bebfd505137c2
Laurentiu Niculae <niculae.laurentiu1@gmail.com>
2023-04-25 13:33:29 +0300

    Add TEMPORARY art type definitions

---
+++ specs-go/v1/artifact.go
@@ -0,0 +1,15 @@
+package v1
+
+type ArtifactManifest struct{}
+
+type Artifact struct {
+	MediaType    string
+	Subject      *Descriptor
+	Blobs        []Descriptor
+	ArtifactType string
+	Annotations  map[string]string
+}
+
+const AnnotationReferrersFiltersApplied = ""
+
+const MediaTypeArtifactManifest = ""
